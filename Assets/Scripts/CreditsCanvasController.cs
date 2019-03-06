6ebb047f7becded9756b58527e43efdd1bdadb50
Roosevelt <treyboyland@aol.com>
2019-03-06 00:06:20 -0600

    Added title screen to the game and a credits section

---
+++ Assets/Scripts/CreditsCanvasController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsCanvasController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject creditsCanvas;
+
+    [SerializeField]
+    Button creditsButton;
+
+    [SerializeField]
+    Button backButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        creditsCanvas.gameObject.SetActive(false);
+    }
+
+    public void HideCanvas()
+    {
+        creditsCanvas.gameObject.SetActive(false);
+        creditsButton.Select();
+    }
+
+    public void ShowCanvas()
+    {
+        creditsCanvas.gameObject.SetActive(true);
+        backButton.Select();
+    }
+}
